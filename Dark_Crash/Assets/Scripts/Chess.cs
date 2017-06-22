324fb79f1a0166b577bf50f8792770b2afdc719a
foolishfool <foolishfool@users.noreply.github.com>
2017-06-22 22:28:27 +1000

    assign neighbour chesses

--- Dark_Crash/Assets/Scripts/Chess.cs
+++ Dark_Crash/Assets/Scripts/Chess.cs
@@ -23,18 +23,55 @@ using UnityEngine;
 
 public class Chess : MonoBehaviour {
 
-    public static Chess instance;  // can be use to pass values among classes in a hihg effieiency
-    private void Awake()
-    {
-        instance = this; //this means current class's instance
-    }
+    public Chess[] chessNeighbour = new Chess[4]; //each chess's neighbour left/right/up/down neigbhour, represent by 0/1/2/3
 
+    internal string strNeighourLeft1 = "Left1";
+    internal string strNeighourLeft2 = "Left2";
+    internal string strNeighourRight1 = "Right1";
+    internal string strNeighourRight2 = "Right2";
+    internal string strNeighourDown1 = "Down1";
+    internal string strNeighourDown2 = "Down2";
+    internal string strNeighourUp1 = "Up1";
+    internal string strNeighourUp2 = "Up2";
 
     // Use this for initialization
     void Start () {
 		
 	}
-	
+    //allocate neighbour
+
+
+    internal void AssignNeighbourNames()
+    {
+        //parameter check
+        if (chessNeighbour == null || chessNeighbour.Length ==0)
+        {
+            Debug.LogError(string.Format("[Chess.cs/AssignNeighbour()] illegal parameter!, please check!"));
+        }
+        //left
+        AssignNeighbourName(strNeighourLeft1, strNeighourLeft2, 0);
+        //right
+        AssignNeighbourName(strNeighourRight1, strNeighourRight2, 0);
+        //up
+        AssignNeighbourName(strNeighourUp1, strNeighourUp2, 0);
+        //down
+        AssignNeighbourName(strNeighourDown1, strNeighourDown2, 0);
+
+    }
+    //assign name to neigbhour chesses
+    internal void AssignNeighbourName(string neighbourString1, string neighbourString2, int neighbourNumber)
+    {
+        if (chessNeighbour[neighbourNumber] != null)
+        {
+            neighbourString1 = chessNeighbour[neighbourNumber].name;
+            if (chessNeighbour[neighbourNumber].chessNeighbour[neighbourNumber] != null)
+            {
+                neighbourString2 = chessNeighbour[neighbourNumber].chessNeighbour[neighbourNumber].name;
+            }
+        }
+
+    }
+
 	// Update is called once per frame
 	void Update () {
 		
--- Dark_Crash/Assets/Scripts/Column.cs
+++ Dark_Crash/Assets/Scripts/Column.cs
@@ -25,7 +25,9 @@ public class Column : MonoBehaviour {
 
     // Column is generated dynamically so there is no need to use static instance
     public int currentColumnNumber = -999; // current column number , -999 is easy to check error
-	// Use this for initialization
+                                           // Use this for initialization
+    internal List<Chess> chessArray = new List<Chess>(); //the chess collection that current column contains
+
 	void Start () {
         for (int row = 0; row < GameManager.instance.IntRowNumber; row++)
         {
@@ -39,17 +41,57 @@ public class Column : MonoBehaviour {
             cloneObj.transform.parent = this.transform;
             //specify the scale of chess
             cloneObj.transform.localScale = new Vector3(GameManager.instance.ChessScale, GameManager.instance.ChessScale, GameManager.instance.ChessScale);
-         
-
-
-
+            //store coloneobj into List<Chess>
+            chessArray.Add(cloneObj.GetComponent<Chess>());
         }
-	
-        
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    //assign neighbour
+    internal void AssignNeighbour()
+    {
+        for (int row = 0; row < chessArray.Count; row++) // list uses count not length
+        {
+            //left
+            if (currentColumnNumber == 0)
+            {
+                chessArray[row].chessNeighbour[0] = null;
+            }
+            else
+            {
+                //********************* difficulty*********************//
+                chessArray[row].chessNeighbour[0] = ColumnManager.instance.colArray[currentColumnNumber - 1].chessArray[row];//left column's chess
+
+            }
+            //right
+
+            if (currentColumnNumber == ColumnManager.instance.colArray.Length -1 )
+            {
+                chessArray[row].chessNeighbour[1] = null;
+            }
+            else
+            {               
+                chessArray[row].chessNeighbour[1] = ColumnManager.instance.colArray[currentColumnNumber + 1].chessArray[row];//right column's chess
+            }
+            //up
+            if (row == 0)
+            {
+                chessArray[row].chessNeighbour[2] = null;
+            }
+            else
+            {             
+                chessArray[row].chessNeighbour[2] = chessArray[row - 1];//up chess in the same column
+            }
+            //down
+            if (row == chessArray.Count -1 ) 
+            {
+                chessArray[row].chessNeighbour[3] = null;
+            }
+            else
+            {   
+                chessArray[row].chessNeighbour[3] = chessArray[row + 1];//down chess in the same column
+
+            }
+        }
+
 }
